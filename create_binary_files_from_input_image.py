8beca832223b1604be2089e960a684ba8397321c
mehrdad <46937343+khatami-mehrdad@users.noreply.github.com>
2020-01-09 11:10:00 -0800

    add regular evaluate function

--- create_binary_files_from_input_image.py
+++ create_binary_files_from_input_image.py
@@ -110,8 +110,10 @@ def dg_main(args):
     # patch_fastrcnn(model)
     model.to(device)
     
-    if args.test_only:
-        evaluate(model, data_loader_test, device=device, bin_folder = args.bin)
+    if args.bin_evaluate:
+        evaluate_bin(model, data_loader_test, device=device, bin_folder = args.bin)
+    elif args.test_only:
+        evaluate(model, data_loader_test, device=device)
     else:
         preprocess_and_save_bin(model, data_loader_test, device=device)
 
@@ -147,7 +149,7 @@ def preprocess_and_save_bin(model, data_loader, device):
     torch.set_num_threads(n_threads)
 
 @torch.no_grad()
-def evaluate(model, data_loader, device, bin_folder ):
+def evaluate_bin(model, data_loader, device, bin_folder ):
 
     n_threads = torch.get_num_threads()
     # FIXME remove this and make paste_masks_in_image run on the GPU
@@ -203,10 +205,47 @@ def evaluate(model, data_loader, device, bin_folder ):
     coco_evaluator.summarize()
     torch.set_num_threads(n_threads)
 
-    
+@torch.no_grad()
+def evaluate(model, data_loader, device):
+    n_threads = torch.get_num_threads()
+    # FIXME remove this and make paste_masks_in_image run on the GPU
+    torch.set_num_threads(1)
+    cpu_device = torch.device("cpu")
+    model.eval()
+    metric_logger = utils.MetricLogger(delimiter="  ")
+    header = 'Test:'
 
+    coco = get_coco_api_from_dataset(data_loader.dataset)
+    iou_types = _get_iou_types(model)
+    coco_evaluator = CocoEvaluator(coco, iou_types)
 
+    for image, targets in metric_logger.log_every(data_loader, 100, header):
+        image = list(img.to(device) for img in image)
+        targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
+        torch.cuda.synchronize()
+        model_time = time.time()
+        outputs = model(image)
+
+        outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
+        model_time = time.time() - model_time
+
+        res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
+        evaluator_time = time.time()
+        coco_evaluator.update(res)
+        evaluator_time = time.time() - evaluator_time
+        metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+
+    # gather the stats from all processes
+    metric_logger.synchronize_between_processes()
+    print("Averaged stats:", metric_logger)
+    coco_evaluator.synchronize_between_processes()
+
+    # accumulate predictions from all images
+    coco_evaluator.accumulate()
+    coco_evaluator.summarize()
+    torch.set_num_threads(n_threads)
+    return coco_evaluator
 
 if __name__ == "__main__":
     import argparse
@@ -225,6 +264,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--evaluate",
         dest="test_only",
+        help="Only test the model",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--bin-evaluate",
+        dest="bin_evaluate",
         help="Only test the model",
         action="store_true",
     )

bde0d924e456ee802194741de30ae12780530ef6
Maziar <mazhej>
2020-02-11 11:59:25 -0800

    ssd related files

--- models.py
+++ models.py
@@ -1,5 +1,28 @@
 from yolo_models import *
 import torchvision.models.detection as detection
+from vision import *
+from vision.ssd.mobilenetv1_ssd import create_mobilenetv1_ssd
+class_names = ['background',
+'aeroplane',
+'bicycle',
+'bird',
+'boat',
+'bottle',
+'bus',
+'car',
+'cat',
+'chair',
+'cow',
+'diningtable',
+'dog',
+'horse',
+'motorbike',
+'person',
+'pottedplant',
+'sheep',
+'sofa',
+'train',
+'tvmonitor']
 
 def load_model(args):
     device = torch.device(args.device)
@@ -13,6 +36,10 @@ def load_model(args):
             model.load_state_dict(torch.load(args.weights, map_location=device)['model'])
         else:  # darknet format
             load_darknet_weights(model, args.weights)
+
+    elif (args.model== 'mobilenet_v1'):
+        model = create_mobilenetv1_ssd(len(class_names), is_test=True)
+
     else:
         model = detection.__dict__[args.model](num_classes=91, pretrained=args.pretrained)
     
